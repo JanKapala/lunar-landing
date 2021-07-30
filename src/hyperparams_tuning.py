17a3418a2edd846b06af6806f1253cd7023a9544
Jan Kapała <jankapala@gmail.com>
2021-07-30 22:48:43 +0200

    set trials, resources and proper hyperparam_mutations dict

--- src/hyperparams_tuning.py
+++ src/hyperparams_tuning.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     MAX_CPU_N = PERC_SYSTEM_LOAD*CPU_N
     MAX_GPU_N = PERC_SYSTEM_LOAD*GPU_N
 
-    TRIALS_N = 100
+    TRIALS_N = 1
 
     cpu_per_trial = MAX_CPU_N/TRIALS_N
     if cpu_per_trial > 1:
@@ -30,14 +30,28 @@ if __name__ == "__main__":
     if gpu_per_trial > 1:
         gpu_per_trial = int(np.floor(gpu_per_trial))
 
+    cpu_per_trial = 1
+    gpu_per_trial = 0
+
     print(f"""
     cpu_per_trial: {cpu_per_trial},
     gpu_per_trial: {gpu_per_trial},
     """)
 
     config = {
+        "replay_buffer_max_size": tune.grid_search([10 ** x for x in range(3, 7)]),
+        "batch_size": tune.grid_search([2 ** x for x in range(4, 10)]),
+        "learning_freq": tune.grid_search([2 ** x for x in range(0, 5)]),
+        "γ": tune.uniform(0.9, 1),
+        "μ_θ_α": tune.loguniform(1e-6, 1e-1),
+        "Q_Φ_α": tune.loguniform(1e-6, 1e-1),
+        "ρ": tune.loguniform(0.5, 0.95),
+        "noise_sigma": tune.uniform(0, 1),
+    }
+
+    hyperparam_mutations = {
         "replay_buffer_max_size": tune.choice([10 ** x for x in range(3, 7)]),
-        "batch_size": tune.choice([2 ** x for x in range(2, 10)]),
+        "batch_size": tune.choice([2 ** x for x in range(4, 10)]),
         "learning_freq": tune.choice([2 ** x for x in range(0, 5)]),
         "γ": tune.uniform(0.9, 1),
         "μ_θ_α": tune.loguniform(1e-6, 1e-1),
@@ -57,8 +71,8 @@ if __name__ == "__main__":
     # Postprocess the perturbed config to ensure it's still valid
     def explore(config):
         clip_limits(config, "replay_buffer_max_size", 10**3, 10**6)
-        clip_limits(config, "batch_size", 2, 10)
-        clip_limits(config, "learning_freq", 1, 32)
+        clip_limits(config, "batch_size", 16, 512)
+        clip_limits(config, "learning_freq", 1, 16)
         clip_limits(config, "γ", 0.9, 1)
         clip_limits(config, "μ_θ_α", 1e-6, 1e-1)
         clip_limits(config, "Q_Φ_α", 1e-6, 1e-1)
@@ -70,7 +84,7 @@ if __name__ == "__main__":
     pbt = PopulationBasedTraining(
         time_attr="training_iteration",
         perturbation_interval=1,
-        hyperparam_mutations=config,
+        hyperparam_mutations=hyperparam_mutations,
         custom_explore_fn=explore,
         resample_probability=0.25,
         synch=True
@@ -85,7 +99,7 @@ if __name__ == "__main__":
         fail_fast=True,
         stop={"training_iteration": 400, "mean_return": 200},
         num_samples=TRIALS_N,
-        resources_per_trial={'cpu': cpu_per_trial, 'gpu': 0}, # cpu_per_trial, gpu_per_trial
+        resources_per_trial={'cpu': cpu_per_trial, 'gpu': gpu_per_trial},
         local_dir=LOG_DIR,
         verbose=ONLY_STATUS_UPDATES,
         reuse_actors=True,
